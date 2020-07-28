71b0daf32526e9fc0564af57560c5a325828e58f
YunHoseon <hosun2776@gmail.com>
2020-07-28 21:05:08 +0900

    no message

--- ★알고리즘/0714/0714/0714.cpp
+++ ★알고리즘/0714/0714/0714.cpp
@@ -1,149 +1,92 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 
 using namespace std;
 
-class ClassMate
+class Box
 {
-	int no;
-	string name;
-	ClassMate *next;
-
 public:
-	ClassMate();
-	~ClassMate();
+	int length;
+	int breadth;
+	int height;
 
-	void AddStudent();
-	void DeleteStudent();
-	void PrintStudent();
+	Box()
+	{
+
+	}
+	Box(int l, int b, int h) { length = l; breadth = b; height = h; }
+	Box(Box &B) { length = B.length; breadth = B.breadth; height = B.height; }
+
+	int getLength() { return length; }
+	int getbreadth() { return breadth; }
+	int getheight() { return height; }
+	long long CalculateVolume() { return length * breadth * height; }
+
+	bool operator<(Box& b)
+	{
+		return (this->CalculateVolume() < b.CalculateVolume());
+	}
+
+	friend ostream& operator<<(ostream& out, const Box& B);
 };
 
-ClassMate *head = NULL;
+ostream & operator<<(ostream & out, const Box & B)
+{
+	{
+		out << B.length << B.breadth << B.height;
+		return out;
+	}
+	// TODO: 여기에 반환 구문을 삽입합니다.
+}
+
+void check2()
+{
+	int n;
+	cin >> n;
+	Box temp;
+	for (int i = 0;i<n;i++)
+	{
+		int type;
+		cin >> type;
+		if (type == 1)
+		{
+			cout << temp << endl;
+		}
+		if (type == 2)
+		{
+			int l, b, h;
+			cin >> l >> b >> h;
+			Box NewBox(l, b, h);
+			temp = NewBox;
+			cout << temp << endl;
+		}
+		if (type == 3)
+		{
+			int l, b, h;
+			cin >> l >> b >> h;
+			Box NewBox(l, b, h);
+			if (NewBox<temp)
+			{
+				cout << "Lesser\n";
+			}
+			else
+			{
+				cout << "Greater\n";
+			}
+		}
+		if (type == 4)
+		{
+			cout << temp.CalculateVolume() << endl;
+		}
+		if (type == 5)
+		{
+			Box NewBox(temp);
+			cout << NewBox << endl;
+		}
+
+	}
+}
 
 int main()
 {
-	int num;
-	ClassMate *clm = new ClassMate;
-
-	while (1)
-	{
-		cout << "메뉴 번호를 입력하세요." << endl << endl;
-		cout << "1. 학생 추가" << endl;
-		cout << "2. 학생 삭제" << endl;
-		cout << "3. 전체 출력" << endl;
-		cout << "4. 종료" << endl;
-
-		cin >> num;
-
-		switch (num)
-		{
-		case 1:
-			clm->AddStudent();
-			break;
-		case 2:
-			clm->DeleteStudent();
-			break;
-		case 3:
-			clm->PrintStudent();
-			break;
-		case 4:
-			exit(0);
-			break;
-
-		default:
-			break;
-		}
-	}
-}
-
-ClassMate::ClassMate()
-{
-}
-
-ClassMate::~ClassMate()
-{
-}
-
-void ClassMate::AddStudent()
-{
-	ClassMate *clm = new ClassMate;
-	ClassMate *tmp = NULL;
-
-	cout << "번호를 입력하세요 : ";
-	cin >> clm->no;
-	cout << "이름을 입력하세요 : ";
-	cin >> clm->name;
-
-	clm->next = NULL;
-
-	if (head == NULL)	//맨 처음 입력일 때
-		head = clm;
-	else
-	{
-		tmp = head;
-
-		if (tmp->next == NULL)	//자료가 한개 있을 때
-		{
-			if (clm->no < tmp->no)
-			{
-				clm->next = tmp;
-
-				if (tmp == head)
-					head = clm;
-			}
-			else
-				tmp->next = clm;
-		}
-		else
-		{
-			while (tmp->next != NULL)	//마지막 자료까지
-			{
-				if (clm->no < tmp->no)
-				{
-					clm->next = tmp;
-
-					if (tmp == head)
-						head = clm;
-				}
-				tmp = tmp->next;
-			}
-		}
-	}
-}
-
-void ClassMate::DeleteStudent()
-{
-	ClassMate *tmp;
-	tmp = head;
-	int dNum;
-
-	cout << "삭제할 학생의 번호 : ";
-	cin >> dNum;
-
-	while (tmp != NULL)
-	{
-		if (dNum == tmp->no)
-		{
-			delete tmp;
-			break;
-		}
-	}
-
-	cout << "해당하는 번호의 학생이 없습니다." << endl;
-}
-
-void ClassMate::PrintStudent()
-{
-	ClassMate *tmp;
-	tmp = head;
-
-	while (head != NULL)
-	{
-		cout << "번호 : " << head->no << endl;
-		cout << "이름 : " << head->name << endl;
-		cout << endl;
-
-		head = head->next;
-	}
-	head = tmp;
+	check2();
 }
